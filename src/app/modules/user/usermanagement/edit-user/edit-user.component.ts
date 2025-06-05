4e88d811c11929f617c739f8594ab47d510bbf0a
AshishYadav43 <ashishyadav.eminence@gmail.com>
2025-06-05 13:09:34 +0530

    edit form update

---
+++ src/app/modules/user/usermanagement/edit-user/edit-user.component.ts
@@ -0,0 +1,74 @@
+import { Component, Inject } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { ApiService } from 'src/app/core/services/api.service';
+
+@Component({
+  selector: 'app-edit-user',
+  templateUrl: './edit-user.component.html',
+  styleUrls: ['./edit-user.component.scss']
+})
+export class EditUserComponent {
+
+  userForm: FormGroup;
+
+  nameControl = new FormControl('', Validators.required);
+  emailControl = new FormControl('', [Validators.required, Validators.email]);
+  mobileControl = new FormControl('', Validators.required);
+  userIdControl = new FormControl('', Validators.required);
+  roleControl = new FormControl('', Validators.required);
+  userTypeControl = new FormControl('', Validators.required);
+
+  roleOptions = [
+  
+  ];
+
+  userTypeOptions = [
+    { label: 'Internal', value: 'internal' },
+    { label: 'External', value: 'external' }
+  ];
+
+  constructor(private fb: FormBuilder,
+        private api: ApiService,
+      public dialogRef: MatDialogRef<EditUserComponent>,
+      private toastr: ToastrService,
+        @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.userForm = this.fb.group({
+      name: this.nameControl,
+      email: this.emailControl,
+      mobile: this.mobileControl,
+      userId: this.userIdControl,
+      role: this.roleControl,
+      userType: this.userTypeControl
+    });
+
+        this.getChildRole();
+
+  }
+
+
+  getChildRole() {
+    this.api.getChildRole().subscribe({
+      next: (res: any) => {
+        if (res.data.length === 0) {
+          this.toastr.error('Please add the child role first');
+          this.dialogRef.close();
+        }
+        this.roleOptions = res.data.map((item: any) => {
+          return { label: item.roleName, value: item.uuid }
+        })
+      }
+    })
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log('Form submitted:', this.userForm.value);
+    } else {
+      this.userForm.markAllAsTouched();
+    }
+  }
+
+}
